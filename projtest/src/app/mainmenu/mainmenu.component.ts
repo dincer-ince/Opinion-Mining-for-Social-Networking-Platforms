c4ca97b6d78df87a4314098b85f446b370be6112
dincer-ince <dincer.ince00@gmail.com>
2021-12-30 14:30:30 +0300

    Revert "Revert "Merge pull request #2 from dincer-ince/revert-1-deneme""
    This reverts commit 31944d4d93f3878338c9005322c2bdfb542d2d0d.

--- projtest/src/app/mainmenu/mainmenu.component.ts
+++ projtest/src/app/mainmenu/mainmenu.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Post } from '../models/post.model';
 import { Comment } from '../models/comment.model';
 import { FormBuilder } from '@angular/forms';
-import { PostService } from '../_services/post.service';
+
+
 @Component({
   selector: 'app-mainmenu',
   templateUrl: './mainmenu.component.html',
@@ -10,9 +11,10 @@ import { PostService } from '../_services/post.service';
 })
 export class MainmenuComponent implements OnInit {
 
-  constructor(private fb:FormBuilder, public service: PostService) { }
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit(): void {
+    this.viewPosts= this.postas.slice();
     this.loadPosts();
   }
 
@@ -21,53 +23,138 @@ export class MainmenuComponent implements OnInit {
 
   })
 
+  Topics:string[] = [
+    "Funny",
+    "Sports",
+    "Programming",
+    "Music"
+  ];
   
   mainMenuOpen:boolean=true;
-  
-  
+  rndComments:Comment[]=[
+    
+    {
+      votes:3,
+      author:"memedma",
+      text:"hahaa"
+    },
+    {
+      votes:6,
+      author:"sude",
+      text:"lollol"
+    },
+    {
+      votes:1,
+      author:"dubcer",
+      text:"skdjsdk"
+    },
+    {
+      votes:3,
+      author:"Kerem",
+      text:"komik degil"
+    },
+    {
+      votes:3,
+      author:"dora",
+      text:"komikmis"
+    },
+    {
+      votes:3,
+      author:"Mert",
+      text:"lsdksld"
+    },
+    {
+      votes:0,
+      author:"",
+      text:""
+    },
+  ]
 
   formPost=this.fb.group({
+    postTopic:"",
     postContent: "",
     postOwner: "Admin",
     postTitle: "",
-    postComments:[this.service.rndComments[0],this.service.rndComments[2]], 
+    postComments:[this.rndComments[0],this.rndComments[2]], 
   })
 
-  
+  postas: Post[] = [
+    {
+      postTopic: this.Topics[0],
+      postContent: "https://i.redd.it/irxao14hqb581.png",
+      postOwner: "Ahmet",
+      postTitle: "funny words",
+      postComments: [this.rndComments[3],this.rndComments[2]],
+      postVotes: 300
+    },
+    {
+      postTopic: this.Topics[0],
+      postContent: "https://64.media.tumblr.com/958f628cb6dfc4913c64b3ad71afaa5b/tumblr_pxgvdvUHgn1ypupneo1_400.gifv",
+      postOwner: "Ahmet",
+      postTitle: "look at this",
+      postComments: [this.rndComments[3],this.rndComments[2]],
+      postVotes: 5
+    },
+    {
+      postTopic: this.Topics[0],
+      postContent: "https://i.imgur.com/96frlHk.jpeg",
+      postOwner: "mehmet",
+      postTitle: "not funy",
+      postComments: [this.rndComments[3],this.rndComments[4]],
+      postVotes: 903
+    }];
 
   Post: Post = {
+    postTopic: "",
     postContent: "filler",
     postOwner: "filler",
     postTitle: "funny words",
-    postComments: [this.service.rndComments[1],this.service.rndComments[5]]
+    postComments: [this.rndComments[1],this.rndComments[5]],
+    postVotes: 123
   };
   
+  viewPosts:Post[];
+
+
   createPost(){
-    this.service.postas.push({
+    this.postas.push({
+      postTopic: this.formPost.value.postTopic,
       postContent: this.formPost.value.postContent,
       postOwner: this.formPost.value.postOwner,
       postTitle: this.formPost.value.postTitle,
-      postComments: [this.service.rndComments[0]]
+      postComments: [this.rndComments[6]],
+      postVotes: 1
     })
-    console.log(this.service.postas)
+    console.log(this.formPost)
     this.mainMenuOpen=true;
+    this.sortByNew();
   }
+  
 
   loadPosts() {
-    this.service.postas.push({
-      postContent: "https://preview.redd.it/ibcc1lcbdn581.jpg?width=640&crop=smart&auto=webp&s=478a1035d046cb7d8bf1ee163409b2d0b2d6de9f",
+    this.postas.push({
+      postTopic: this.Topics[0],
+      postContent: "https://i.imgur.com/S2lYrKD.gif",
       postOwner: "carsi",
       postTitle: "yeter artuj",
-      postComments: [this.service.rndComments[1],this.service.rndComments[5]]
+      postComments: [this.rndComments[1],this.rndComments[5]],
+      postVotes: 467
     })
+    this.sortByNew()
   }
-  sort() {
-    this.service.postas.reverse();
+  sortByNew() {
+    this.viewPosts=this.postas;
+    this.viewPosts.reverse();
+  }
+  sortByBest(){
+    this.viewPosts=this.postas;
+    this.viewPosts.sort((a, b) => (a.postVotes > b.postVotes ? -1 : 1))
   }
 
   createComment(){
     var tempComment:Comment={votes:1,author:"Admin",text:this.formComment.value.commentText};
-    this.Post.postComments.push(tempComment);
     this.formComment.reset();
+    this.Post.postComments.unshift(tempComment);
+    
   }
 }
